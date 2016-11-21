8c0c02ad92eec7708afa3c8aab9c5eec3ee79b96
Bert Radke <taseroth@gmail.com>
2016-11-21 09:39:06 +0100

    generated via spring initializer

---
+++ src/main/java/org/faboo/test/ParallelIntegrationApplication.java
@@ -0,0 +1,12 @@
+package org.faboo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelIntegrationApplication.class, args);
+	}
+}
